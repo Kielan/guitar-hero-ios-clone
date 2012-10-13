bb061d64d9824a6536d340a8bbf4ea818dca1e99
Ken <thanhcong.bachngoc@gmail.com>
2012-10-12 18:59:31 -0700

    Blank iPhone project.

---
+++ FaceGuitar-iPhone/FaceGuitar-iPhone/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FaceGuitar-iPhone
+//
+//  Created by Ken on 10/12/12.
+//  Copyright (c) 2012 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
