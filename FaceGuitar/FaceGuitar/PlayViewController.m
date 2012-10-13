ebfc50a968a65a0e0a4a797fd2d1d016c6474789
YangShun <mr.tay.yang.shun@gmail.com>
2012-10-12 18:56:04 -0700

    ipad app put in

---
+++ FaceGuitar/FaceGuitar/PlayViewController.m
@@ -0,0 +1,49 @@
+//
+//  PlayViewController.m
+//  FaceGuitar
+//
+//  Created by YangShun on 12/10/12.
+//  Copyright (c) 2012 YangShun. All rights reserved.
+//
+
+#import "PlayViewController.h"
+
+@interface PlayViewController () {
+    IBOutlet UIImageView *col1;
+    IBOutlet UIImageView *col2;
+    IBOutlet UIImageView *col3;
+    IBOutlet UIImageView *col4;
+}
+
+@end
+
+@implementation PlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
